964a7c17b5d7df8fbe0a9cad33fe1e9af8abae50
Priyankalk1 <Priyankalk@maveric-systems.com>
2022-08-23 09:47:46 +0530

    Initial commit

---
+++ src/main/java/com/maveric/mavericbankwebapp/MavericBankWebappApplication.java
@@ -0,0 +1,13 @@
+package com.maveric.mavericbankwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavericBankWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavericBankWebappApplication.class, args);
+	}
+
+}
